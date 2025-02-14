c869b9a9e8d552cece8dbc010d34ed9cd59fc83a
Hobomilo <hobomilo2007@gmail.com>
2025-02-14 14:45:44 -0700

    Update MusicPlayer.js

--- MusicPlayer.js
+++ MusicPlayer.js
@@ -65,11 +65,11 @@ function mouseClicked() {
         const popupMenu = document.getElementById('popupMenu');
         popupMenu.style.display = 'block';
         // Change these if it looks weird
-        popupMenu.style.rectMode = CENTER;
-        popupMenu.style.left = `${overlayX}px`;
-        popupMenu.style.top = `${overlayY}px`;
-        popupMenu.style.width = `${overlayWidth}px`;
-        popupMenu.style.height = `${overlayHeight}px`;
+        popupMenu.style.rectMode = CORNER;
+        popupMenu.style.left = overlayX;
+        popupMenu.style.top = overlayY;
+        popupMenu.style.width = overlayWidth;
+        popupMenu.style.height = overlayHeight;
         popupMenu.style.position = 'absolute';
         popupMenu.style.zIndex = '1000';
         console.log('popup displayed');
